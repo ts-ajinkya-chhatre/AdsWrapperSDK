6416b184ff2b2fdc86f240d8a7a92cc0f00af8d7
ts-ajinkya-chhatre <ts-ajinkya.chhatre@rakuten.com>
2022-01-31 13:20:31 +0530

    Initial project setup

---
+++ AdsWrapperSDKExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdsWrapperSDKExample
+//
+//  Created by Chhatre, Ajinkya on 31/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
